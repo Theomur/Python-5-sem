118cdac835ea785f0874c53fb30c69345d983480
Theomur <kan2003spb@gmail.com>
2024-02-21 23:19:18 +0300

    v2 test

--- Projects/Colours/Colours.py
+++ Projects/Colours/Colours.py
@@ -1,26 +1,21 @@
+import cv2
+import numpy as np
 import time
 
-from PIL import Image
+# Открытие изображения
+img = cv2.imread('lena2.png')
+
+# Преобразование изображения в формат BGR
+img = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 
 # старт таймера
 start_time = time.time()
 
-# Открытие изображения
-img = Image.open("lena.png")
+# Преобразование изображения в массив numpy
+pixels = np.array(img)
 
-# Преобразование изображения в массив пикселей
-pixels = img.load()
-
-# Создание словаря для подсчета цветов
-color_count = {}
-
-# Проход по всем пикселям изображения
-for y in range(img.height):
-    for x in range(img.width):
-        # Получение цвета пикселя
-        color = pixels[x, y]
-        # Добавление цвета в словарь или увеличение счетчика
-        color_count[color] = color_count.get(color, 0) + 1
+# Подсчет уникальных цветов
+unique_colors, counts = np.unique(pixels.reshape(-1, pixels.shape[-1]), axis=0, return_counts=True)
 
 # конец таймера + вычисление времени
 end_time = time.time()
@@ -28,4 +23,4 @@ calculated_time = end_time - start_time
 
 # Вывод количества уникальных цветов и времени выполнения
 print(f"Время выполнения: {calculated_time} секунд")
-print(f"Уникальных цветов: {len(color_count)}")
+print(f"Уникальных цветов: {len(unique_colors)}")
